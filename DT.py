0792667a952480a584d584b7e18238cd37a0a066
IFRA NOOR <147080949+ifranoor1@users.noreply.github.com>
2023-11-26 11:33:47 +0000

    corr with target

--- DT.py
+++ DT.py
@@ -9,3 +9,16 @@ corr=iris_df.corr()
 print(corr)
 import seaborn as sns
 sns.heatmap(corr, annot=True, cmap="crest")
+#setting a threshold of 50% those fearure whose correlation with target variable(price) is less than or equal to 0.5 will drop
+cor_target = abs(corr["Target"])
+
+relevant_features = cor_target[cor_target>0.7]
+#it will print those feature variable that has high correlation with price
+print(relevant_features)
+rel_feature= iris_df
+rel_feature_heatmap=rel_feature[['sepal length (cm)',
+'petal length (cm)',
+'petal width (cm)' ,
+'Target'   ]]
+corr_rel_feature= rel_feature_heatmap.corr()
+print(corr_rel_feature)
