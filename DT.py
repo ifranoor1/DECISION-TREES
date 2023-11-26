9188a892b4703d1fcec446063a57fc5aca83e330
IFRA NOOR <147080949+ifranoor1@users.noreply.github.com>
2023-11-26 11:32:28 +0000

    find corr and heatmap

--- DT.py
+++ DT.py
@@ -5,3 +5,7 @@ import pandas as pd
 #create a dataframe for iris dataset
 iris_df=pd.DataFrame(data=iris.data, columns=iris.feature_names)
 iris_df['Target']=iris.target
+corr=iris_df.corr()
+print(corr)
+import seaborn as sns
+sns.heatmap(corr, annot=True, cmap="crest")
