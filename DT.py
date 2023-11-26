3de4f229f86927fcbb1e6ce0e9d45be834548fde
IFRA NOOR <147080949+ifranoor1@users.noreply.github.com>
2023-11-26 11:37:13 +0000

    apply decision classifier

--- DT.py
+++ DT.py
@@ -31,3 +31,11 @@ y=np.array(y).reshape(-1,1)
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y,
 test_size=0.3, random_state=42)
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+clf = DecisionTreeClassifier(criterion='entropy')
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
